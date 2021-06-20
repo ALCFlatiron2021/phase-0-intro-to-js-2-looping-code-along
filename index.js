48e023f95d3f7e6c2a5e773b48af2a862f768fb9
Alan Cinsavich <alan.cimsavich@gmail.com>
2021-06-20 13:22:11 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+function writeCards (cards) { 
+  let Thankyou = []
+  for (let i = 0; i < cards.length; i++) {
+   Thankyou.push (`Thank you, ${cards[i]}, for the wonderful surprise gift!`)
+    debugger;
+  }
+
+  return Thankyou;
+}
+
+writeCards(cards); 
+
+function countDown () {
+  let countDown = 0;
+  while (countDown < 11) 
+    console.log(countDown++);
+  }
